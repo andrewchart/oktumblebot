ecdfa335e082a0196ba043557c5044ba0721a5cb
Andrew Chart <achart@hotmail.com>
2016-02-28 19:38:44 +0000

    Initial commit

--- index.php
+++ index.php
@@ -13,24 +13,6 @@
 
 
 
-//
-// Ahhh, come on!
-// No need to be insulted
-// I *love* that you are allowed to express yourself on the web
-// 		It's /great/ that you got that off your chest
-// 			No - really - I'm sure it was very cathartic for you
-//
-// If a tweet falls in the forest and nobody is there to hear it...
-// ..
-// .. does it make a sound?
-// 
-// .. well, no probably not.. I've got to go now and find some more
-//
-// 
-//
-// If it makes you feel any better, this author's tweets... they don't get much love either!
-// @AndrewChart
-//
 
 
 ?>
